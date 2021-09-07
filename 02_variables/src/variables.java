79d152d735ec9f3457e6d18a6fa941d9a3d9b9e6
DevGuadalajara <a21310422@ceti.mx>
2021-09-07 07:18:14 -0500

    Added pet's information

--- 02_variables/src/variables.java
+++ 02_variables/src/variables.java
@@ -9,7 +9,22 @@ public class variables
         int yearsOfExperience = 1;
         int age = 29;
 
-        System.out.println("Hello! my name is " + name + " " + lastName + " I am " + age + " years old :D");
-        System.out.println("and I am a " + jobPosition + " I have " + yearsOfExperience + " years of experience");
+        System.out.println( "Hello! my name is " + name + " " + lastName + " I am " + age + " years old :D" );
+        System.out.println( "and I am a " + jobPosition + " I have " + yearsOfExperience + " years of experience" );
+
+        String pet = "Dog";
+        String petName = "Fari";
+        byte petHeight = 40;
+        short petWeight = 8;
+        double petAge = 7.3;
+        char petBloodType = '0';
+
+        System.out.println( "Pet's information:" );
+        System.out.println( "Name: " + petName );
+        System.out.println( "Height: " + petHeight );
+        System.out.println( "Weight:" + petWeight );
+        System.out.println( "Age: " + petAge );
+        System.out.println( "Blood type: " + petBloodType );
+
     }
 }
