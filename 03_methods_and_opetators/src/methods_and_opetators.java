6f0f5e9d72f7ef9227baa4376cdbbc7986ae157c
DevGuadalajara <a21310422@ceti.mx>
2021-09-07 07:40:51 -0500

    Added /03_methods_and_opetators

---
+++ 03_methods_and_opetators/src/methods_and_opetators.java
@@ -0,0 +1,20 @@
+public class methods_and_opetators
+{
+    public static void main (String [] args)
+    {
+        // Basic operators +-*/%
+        int oranges = 92;
+        int apples = 58;
+        int grapes = 25;
+        int totalFruits = oranges + apples + grapes;
+
+        System.out.println( "Oranges: " + oranges );
+        System.out.println( "Apples: " + apples );
+        System.out.println( "Grapes: " + grapes );
+        System.out.println( "Fruits in total: " + totalFruits );
+
+        double orangePrice = 1.7;
+        double applePrice = 0.99;
+        double grapePrice = 0.24;
+    }
+}
