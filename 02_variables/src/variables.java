6706c29e74eb8f5fd5b966b7fbe6a2940bbd5982
DevGuadalajara <a21310422@ceti.mx>
2021-09-07 07:02:14 -0500

    Added /02_variables

---
+++ 02_variables/src/variables.java
@@ -0,0 +1,15 @@
+public class variables
+{
+    public static void main ( String[] args )
+    {
+        String name = "José";
+        String lastName = "Gómez";
+        String jobPosition = "Tester Automation with Selenium Java";
+
+        int yearsOfExperience = 1;
+        int age = 29;
+
+        System.out.println("Hello! my name is " + name + " " + lastName + " I am " + age + " years old :D");
+        System.out.println("and I am a " + jobPosition + " I have " + yearsOfExperience + " years of experience");
+    }
+}
