da6ac5f02e03feb08d88af316d80fe16ee7f2db0
DevGuadalajara <a21310422@ceti.mx>
2021-09-13 05:16:30 -0500

    Added some if statements examples

--- 04_RelationalOperators_if_Statement/src/if_statement.java
+++ 04_RelationalOperators_if_Statement/src/if_statement.java
@@ -1,7 +1,29 @@
 public class if_statement
 {
-    public static void main (String [] args)
+    public static void main ( String [] args )
     {
+        String dogName = "Fari";
+        boolean isAlive = true;
 
+        if ( isAlive == true )
+        {
+            System.out.println( "woof woof!" );
+        }
+        else
+        {
+            System.out.println( "R.I.P" );
+        }
+
+        String owner = "José";
+        double money = 32000;
+
+        if ( money >= 500 )
+        {
+            System.out.println( "You should buy " + dogName + "a ball" );
+        }
+        else
+        {
+            System.out.println( "Go to the bank and ask for a loan" );
+        }
     }
 }
